aa45d4db05e0c0738b3e2e533e83b32f582b043e
rodrigo.perez <190308@utags.edu.mx>
2020-11-30 16:03:50 -0600

    Se agregaron formularios con validaciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,30 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ROUTES } from './app.routes';
 
+// Para trabajar con formularios
+import { FormsModule } from '@angular/forms';
+// Para trabajar con formularios reactivos
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { BooksListComponent } from './components/books-list/books-list.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { CusuariosComponent } from './components/cusuarios/cusuarios.component';
+import { ClibrosComponent } from './components/clibros/clibros.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BooksListComponent,
-    NavbarComponent
+    NavbarComponent,
+    CusuariosComponent,
+    ClibrosComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(ROUTES)
+    RouterModule.forRoot(ROUTES),
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { BooksListComponent } from './components/books-list/books-list.component';
+import { ClibrosComponent } from './components/clibros/clibros.component';
+import { CusuariosComponent } from './components/cusuarios/cusuarios.component';
 
 export const ROUTES: Routes = [
-    {path: 'books-list', component: BooksListComponent},
+    { path: 'books-list', component: BooksListComponent },
+    { path: 'cusuarios', component: CusuariosComponent },
+    { path: 'clibros', component: ClibrosComponent },
     { path: '**', pathMatch: 'full', redirectTo: 'books-list' }
 ]
